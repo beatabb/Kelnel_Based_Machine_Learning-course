926d6ee4a8453e6e02f556050e3ee2ae17692362
baczynska <20beata17@gmail.com>
2021-12-01 18:08:45 +0100

    with validation split

--- svm.py
+++ svm.py
@@ -11,66 +11,73 @@ import pickle
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, BaggingClassifier
 
-
-#num of svms
-N = 1
-
+# num of svms
+N = 10
 
 ### DATA ###
 df = pd.read_csv('dataset/kdd_train.csv')
-d_train= np.array(df)
+d_train = np.array(df)
+d_train_X = d_train[:, 0:-1]
+d_train_y = d_train[:, -1]
+X_train, X_validation, y_train, y_validation = train_test_split(d_train_X, d_train_y, test_size=0.2)
 test = np.array(pd.read_csv('dataset/kdd_test.csv'))
+d_test_X = test[:, 0:-1]
+d_test_y = test[:, -1]
+
 #############
 
 def prepare_data(data):
-    #shuffle data on every run
-    np.random.shuffle(data)
-    #split in N chunks
+    # split in N chunks
     new_data = np.array_split(data, N)
     return np.array(new_data)
 
-#create multiple svms and store in array
+
+# create multiple svms and store in array
 def get_mult_svm():
-    SVMs = [SVC(C=1, kernel='rbf', gamma=10) for i in range(N)]
+    SVMs = [SVC(C=(i + 1) * 10, kernel='linear') for i in range(N)]
     return SVMs
 
-#train every svm on the subset
-#cnt is number of rounds
-def train(svms, train_data, cnt):
-    for c in range(cnt):
-        # shuffle data random for every new training round
-        data = prepare_data(train_data)
-        for i in range(len(svms)):
-            d = data[i]
-            X = d[:, 0:-1]
-            y = d[:,-1] 
-            svm = svms[i]
-            start_time = time.time()
-            print("Training of SVM nr " + str(i) + " in round " + str(c+1) + " started at ... " + str(time.time()))
-            svm.fit(X,y)
-            print("--- %s seconds ---" % (time.time() - start_time))
+
+def train_baggingClf(X, y):
+    print("start training")
+    clf = BaggingClassifier(base_estimator=SVC(kernel="linear", verbose=True),
+                            n_estimators=10, random_state=0)
+    clf.fit(X, y)
+    return clf
+
+
+# train every svm on the subset
+def train(svms):
+    data_X = prepare_data(X_train)
+    data_y = prepare_data(y_train)
+    for i in range(len(svms)):
+        X = data_X[i]
+        y = data_y[i]
+        svm = svms[i]
+        start_time = time.time()
+        print("Training of SVM nr " + str(i) + " in round started at ... " + str(time.time()))
+        svm.fit(X, y)
+        print("--- %s seconds ---" % (time.time() - start_time))
     return svms
 
 
-def predict(svms, test):
-    X = test[:, 0:-1]
-    y = test[:,-1] 
+def predict(svms, test_X, test_y):
     y_pred = []
-    for elem in range(len(X)):
+    for elem in range(len(test_X)):
         pred = []
         for i in range(len(svms)):
-            #prediction of svm nr i
-            p = svms[i].predict(X[elem].reshape(1, -1))
+            # prediction of svm nr i
+            p = svms[i].predict(test_X[elem].reshape(1, -1))
             pred.append(int(p.squeeze()))
             # vote which is the actual predition
         pred = np.bincount(pred).argmax()
         y_pred.append(pred)
-    y_pred = np.asarray(y_pred)  
-    print("Accuracy:", accuracy_score(y, y_pred))
+    y_pred = np.asarray(y_pred)
+    print("Accuracy:", accuracy_score(test_y, y_pred))
     ### confusion matrix
-    cm = confusion_matrix(y, y_pred)
+    cm = confusion_matrix(test_y, y_pred)
     fig, ax = plt.subplots(figsize=(8, 8))
     ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=["normal", "DoS", "Probe", "U2R", "R2L"]
                            ).plot(ax=ax)
@@ -83,6 +90,7 @@ def save_models(models):
         filename = 'models/svm_{}.sav'.format(i)
         pickle.dump(models[i], open(filename, 'wb'))
 
+
 def load_models():
     svms = []
     for i in range(N):
@@ -91,30 +99,36 @@ def load_models():
     return svms
 
 
-def train_tree(data):
-    X = data[:, 0:-1]
-    y = data[:,-1] 
+def train_tree(X, y):
     clf = RandomForestClassifier(max_depth=40, random_state=10, verbose=1)
-    clf.fit(X,y)
+    clf.fit(X, y)
     return clf
 
-def pred_tree(test,clf):
-    X = test[:, 0:-1]
-    y = test[:,-1] 
+
+def pred_tree(X, y, clf):
     y_pred = clf.predict(X)
     print("Accuracy:", accuracy_score(y, y_pred))
 
 
-#create N svms and return them in list
+def pred_bag(X, y, clf):
+    print("predict")
+    y_pred = clf.predict(X)
+    print("Accuracy:", accuracy_score(y, y_pred))
+
+
+# create N svms and return them in list
 svms = get_mult_svm()
-print(len(d_train))
-#train N svms in cnt rounds
-train(svms, d_train, cnt=1)
-#save_models(svms)
+# print(len(d_train))
+# train N svms in cnt rounds
+train(svms)
+# save_models(svms)
 
-#svms = load_models()
-#save_models(svms)
-predict(svms, test=test)
+# svms = load_models()
+# save_models(svms)
+predict(svms, X_validation, y_validation)
 
-#clf = train_tree(d_train)
-#pred_tree(test, clf)
+# clf = train_tree(d_train)
+# pred_tree(test, clf)
+
+# clf = train_baggingClf(d_train)
+# pred_bag(test, clf)
